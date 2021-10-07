febd6a3b5289479f40f6bb4f38c1a018c4e6d851
Bartłomiej Karbownik <bkar@netcompany.com>
2021-10-07 14:07:04 +0200

    Fix configuration for local debugging

--- App/Controllers/ValuesController.cs
+++ App/Controllers/ValuesController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+﻿using System.Collections.Generic;
 using System.Web.Http;
 
 namespace App.Controllers
@@ -12,7 +8,7 @@ namespace App.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new[] {"value1", "value2"};
         }
 
         // GET api/values/5
