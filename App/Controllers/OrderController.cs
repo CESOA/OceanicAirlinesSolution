552349c785702938e51d0a745a0e31f0a070656b
Zuzanna Ćwiklińska <zuzc@netcompany.com>
2021-10-07 17:59:25 +0200

    order entity update

--- App/Controllers/OrderController.cs
+++ App/Controllers/OrderController.cs
@@ -13,12 +13,12 @@ namespace App.Controllers
         {
             orderRepository = new OrderRepository();
         }
-        public Order CreateOrder(string routeOption, string userInfo,string cardholdersName, 
+        public Order CreateOrder(RouteOption selectedRoute, User user, string cardholdersName, 
                 string cardNo, string expirationDate, string cvv, string name, string email, 
                 string phoneNo, string street, string streetNo, string zipCode, string city, 
                 string country, string comment)
         {
-            var order = new Order(routeOption, userInfo,
+            var order = new Order(selectedRoute, user, 
                 new Payment(cardholdersName, cardNo, expirationDate, cvv),
                 new Address(name, email, phoneNo, street, streetNo, zipCode, city, country), 
                 comment);
--- App/Models/Order.cs
+++ App/Models/Order.cs
@@ -4,12 +4,11 @@ namespace App.Models
 {
     public class Order
     {
-        // private User UserInfo { get; set; } todo!
         // private RouteOption RouteOption { get; set; } todo!
-        public Order(string routeOption, string userInfo, Payment paymentInfo, Address deliveryAddress, string comment)
+        public Order(RouteOption selectedRoute, User userInfo, Payment paymentInfo, Address deliveryAddress, string comment)
         {
             Id = Guid.NewGuid();
-            RouteOption = routeOption;
+            SelectedRoute = selectedRoute;
             UserInfo = userInfo;
             PaymentInfo = paymentInfo;
             DeliveryAddress = deliveryAddress;
@@ -17,8 +16,8 @@ namespace App.Models
         }
 
         private Guid Id { get;}
-        private string RouteOption { get; set; }
-        private string UserInfo { get; set; }
+        private RouteOption SelectedRoute { get; set; }
+        private User UserInfo { get; set; }
         private Payment PaymentInfo { get; set; }
         private Address DeliveryAddress { get; set; }
         private string Comment { get; set; }
